ecd45ec691225a2c8f3ea55a8d90a9c088f54a2c
Suryansh Kumar <suryansh7250@gmail.com>
2024-08-02 20:59:31 +0530

    Video - 4 - Handling Routes, Middleware, Error handling using express

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+
+app.use(function(req,res,next) {
+    console.log('Middleware Worked!');
+    next();
+});
+app.get("/", function (req, res){
+	res.send("Hello World");
+});
+app.use(function (req,res,next) {
+    console.log("Middleware before Profile Page");
+    next();
+})
+app.get("/profile", function (req,res,next) {
+    return next(new Error('Something went wrong'));
+})
+app.use((err, req, res, next) => {
+  console.error(err.stack)
+  res.status(500).send('Something broke!')
+})
+
+app.listen(3000);
