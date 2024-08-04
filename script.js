a839d71298bc274255a9c80d7f5fb2b0f25c752d
Suryansh Kumar <suryansh7250@gmail.com>
2024-08-04 12:09:50 +0530

    Video - 5 - Express

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 const express = require("express");
 const app = express();
 
-app.use(function(req,res,next) {
-    console.log('Middleware Worked!');
-    next();
-});
-app.get("/", function (req, res){
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.get("/", function (req, res) {
 	res.send("Hello World");
 });
-app.use(function (req,res,next) {
-    console.log("Middleware before Profile Page");
-    next();
-})
-app.get("/profile", function (req,res,next) {
-    return next(new Error('Something went wrong'));
-})
+app.use(function (req, res, next) {
+	console.log("Middleware before Profile Page");
+	next();
+});
+app.get("/profile", function (req, res, next) {
+	return next(new Error("Something went wrong"));
+});
 app.use((err, req, res, next) => {
-  console.error(err.stack)
-  res.status(500).send('Something broke!')
-})
+	console.error(err.stack);
+	res.status(500).send("Something broke!");
+});
 
 app.listen(3000);
